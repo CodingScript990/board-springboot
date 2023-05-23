560d30b70e3143dd55447277bf75d07b854d499e
trte135 <trte135@naver.com>
2023-05-23 23:50:54 +0900

    SpringBoardFE - SignUp View Desgin
    - input value 값을 받아와주는 handleChange 함수를 사용함
    - useState를 사용하여 값을 받아와주는 작업을함
    - MUI 라이브러리를 이용하여 버튼, 박스등 디자인함
    - axios를 활용하여 HTTP통신을 시도함
    - 회원가입 버튼 누를시 onClick이벤트 발생시켜 input value를 DB에 저장해주는 작업을 해줌

--- src/views/Athentication/SignUp/index.tsx
+++ src/views/Athentication/SignUp/index.tsx
@@ -1,40 +1,65 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
+
 export default function SignUp() {
 
   // useState
-  const [requestResult, setRequestResult] = useState<string>('');
+  const [userEmail, setUserEmail] = useState<string>('');
+  const [userPassword, setUserPassword] = useState<string>('');
+  const [userPasswordCheck, setUserPasswordCheck] = useState<string>('');
+  const [userNickname, setUserNickname] = useState<string>('');
+  const [userPhoneNumber, setUserPhoneNumber] = useState<string>('');
+  const [userAddress, setUserAddress] = useState<string>('');
+  const [userAddressDetail, setUserAddressDetail] = useState<string>('');
   // signUpHandler
   const signUpHandler = () => {
       // data const
       const data =
       {
-        "userEmail":"test@gmail.com",
-        "userPassword":"test1234",
-        "userPasswordCheck":"test1234",
-        "userNickname":"Jhon Weck",
-        "userPhoneNumber":"010-1234-5678",
-        "userAddress":"USA New York",
-        "userAddressDetail":"Cal been"
+        userEmail,
+        userPassword,
+        userPasswordCheck,
+        userNickname,
+        userPhoneNumber,
+        userAddress,
+        userAddressDetail,
       };
       // post request
       axios.post('http://localhost:4000/api/auth/signUp', data)
       .then((rep) => {
-        // 성공하면?
-        setRequestResult('Successfull!');
+        
       })
       .catch((err) => {
-        // 실패하면?
-        setRequestResult('Failed!');
+        
       });
   };
 
   return (
-    <div>
-      <h3>{requestResult}</h3>
-      {/* button 누를시 실행[함수] */}
-      <button onClick={() => signUpHandler()}>회원가입</button>
-    </div>
+    <Card sx={{ minWidth: 275, maxWidth: "50vw" }}>
+      <CardContent>
+        <Box>
+          <TextField fullWidth label="Email" type="email" variant="standard" onChange={(e) => setUserEmail(e.target.value)} />
+          <TextField fullWidth label="PW" type="password" variant="standard" onChange={(e) => setUserPassword(e.target.value)} />
+          <TextField fullWidth label="PW Check" type="password" variant="standard" onChange={(e) => setUserPasswordCheck(e.target.value)} />
+          <TextField fullWidth label="Nick Name" variant="standard" onChange={(e) => setUserNickname(e.target.value)} />
+          <TextField fullWidth label="Phone Number" variant="standard" onChange={(e) => setUserPhoneNumber(e.target.value)} />
+          <TextField fullWidth label="Address" variant="standard" onChange={(e) => setUserAddress(e.target.value)} />
+          <TextField fullWidth label="Detail Address" variant="standard" onChange={(e) => setUserAddressDetail(e.target.value)} />
+        </Box>
+      </CardContent>
+      <CardActions>
+        {/* button 누를시 실행[함수] */}
+        <Button fullWidth onClick={() => signUpHandler()} variant="contained">회원가입</Button>
+      </CardActions>
+    </Card>
+    
   );
 }
