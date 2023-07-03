96a3326f49f97dab8b2f99f734759d2aa705039d
Minwookim <trte135@naver.com>
2023-07-03 22:02:19 +0900

    feat.July.3.2023 BoardFE - [Authentication(token, cookies)]
    - Content : 
       1. BoardMain Directory add
       2. index.tsx file add
       3. response http통신을 위한 axios get method를 통해서 token을 부여하여 로그인 성공 시 값을 표현함!

--- src/views/layouts/MainLayout/index.tsx
+++ src/views/layouts/MainLayout/index.tsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navigation from '../../Authentication/Navigation';
 import Authentication from '../../Authentication';
+import BoardMain from '../../BoardMain';
+import { useUserStore } from '../../../stores';
+import { useCookies } from 'react-cookie';
+import axios from 'axios';
 
 export default function MainLayout() {
+  // useState
+  const [boardResponse, setBoardResponse] = useState<string>('');
+  // cookies
+  const [cookies] = useCookies();
+  // useStore
+  const {user} = useUserStore();
+
+  // getBoard => async
+  const getBoard = async (token: string) => {
+    // requestOption value
+    const requestoption = {
+      // headers : {token 보여주는 작업}
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    };
+    // response => http get method
+    await axios.get('http://localhost:4000/api/board/', requestoption).then(
+      (response => {
+        // setBoardRespons => data
+    setBoardResponse(response.data);
+      })
+    ).catch(err => null);
+    
+  };
+
+  // useEffect => user
+  useEffect(() => {
+    // token
+    const token = cookies.token;
+    if(token) getBoard(token);
+  }, [user])
+
   return (
     <>
         <Navigation />
-        <Authentication />
+        {boardResponse ? (<BoardMain />) : (<Authentication />)}
     </>
   )
 }
